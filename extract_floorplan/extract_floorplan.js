6abfd93657ca3b0a7e6b3bde5080a26c6bb1c865
Ulrich Krispel <ulrich.krispel@vc.fraunhofer.at>
2015-10-01 14:49:35 +0200

    d3 playground

--- extract_floorplan/extract_floorplan.js
+++ extract_floorplan/extract_floorplan.js
@@ -1,5 +1,26 @@
-﻿      //The data for our line
-      var polygon = [ [1,5], [20,20], [40,10], [60,40], [80,5], [100,60] ];
+﻿
+  // create floor plan: process wall jsons
+  //var wall_polygons = [];
+  
+  //walljson.Walls.for_each(function (wall) {
+    
+  //});
+      
+      var polygons = 
+      [
+        [ 
+          [0,0],
+          [50,10],
+          [60,50],
+          [10,40]
+        ],
+        [ 
+          [10,50],
+          [60,60],
+          [70,100],
+          [20,80]
+        ]
+      ];
 
       //This is the accessor function we talked about above
       var lineFunction = d3.svg.line()
@@ -11,10 +32,19 @@
       var svgContainer = d3.select("body").append("svg")
                                           .attr("width", 200)
                                           .attr("height", 200);
-
-      //The line SVG Path we draw
-      var lineGraph = svgContainer.append("path")
-                                  .attr("d", lineFunction(polygon))
-                                  .attr("stroke", "blue")
-                                  .attr("stroke-width", 2)
-                                  .attr("fill", "none");
+                                          
+                                          
+      var plg = svgContainer.selectAll("polygon")
+                      .data(polygons)
+                      .enter().append("polygon")
+                      .attr("points",function(d) { return d.map(function(d) { return d.join(",");}).join(" ") })
+                      .attr("stroke", "black").attr("stroke-width",2).attr("fill", "lightgray");
+                      
+      console.log(plg);
+            
+////The line SVG Path we draw
+      //var lineGraph = svgContainer.append("path")
+      //                            .attr("d", lineFunction(polygon))
+      //                            .attr("stroke", "blue")
+      //                            .attr("stroke-width", 2)
+      //                            .attr("fill", "none");
